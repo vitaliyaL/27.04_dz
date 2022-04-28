189be2d8f8b0f790b526d6207cd98d0cb17b0a5b
vitaliyaL <vita0623@mail.ru>
2022-04-28 13:08:05 +0300

    feat: task2 added

--- script/app.js
+++ script/app.js
@@ -7,6 +7,28 @@ if (userName && userSurname) {
   console.log(`Добро пожаловать, ${login}!`);
 } else if (userName && !userSurname) {
   console.log(`Добро пожаловать, ${userName}!`);
-} else{
-    console.log(`Добро пожаловать, Noname!`);
+} else {
+  console.log(`Добро пожаловать, Noname!`);
+}
+
+function isCyrillic(str) {
+  return /[а-я]/i.test(str);
+}
+function isInteger(num) {
+    return (num ^ 0) === num;
+}
+const str = prompt("Введите любое число:");
+console.log(`Вы ввели: ${str}.`)
+const num= +str;
+if (!isNaN(num)) {
+    let str='Введенное вами число ';
+    (num>=100) ? str+='больше ста, ':str+='меньше ста, ';
+    (num%2===0) ?  str+='четное, ':str+='нечетное, ';
+    isInteger(num) ? str+='недробное, ': str+='дробное, ';
+    (num>=0) ? str+='положительное.': str+='отрицательное.';
+    console.log(str)
+} else {
+  isCyrillic(str)
+    ? console.log(`Вместо числа введен текст на кирилице.`)
+    : console.log(`Вместо числа введен текст на латинице.`);
 }
